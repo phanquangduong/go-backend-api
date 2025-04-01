db09e5050faf221c694101b6f4a03691fa59867c
phanquangduong2002 <phanquangduong2002@gmail.com>
2025-04-01 15:33:57 +0700

    setup goose and sqlc

---
+++ internal/initialize/mysqlc.go
@@ -0,0 +1,80 @@
+package initialize
+
+import (
+	"database/sql"
+	"fmt"
+	"go/go-backend-api/global"
+	"go/go-backend-api/internal/po"
+	"time"
+
+	"go.uber.org/zap"
+	"gorm.io/gen"
+)
+
+func checkErrorPanicC(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+	}
+}
+
+func InitMysqlC() {
+	m := global.Config.Mysql
+
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+	db, err := sql.Open("mysql", s)
+
+	checkErrorPanic(err, "InitMysql initialization error")
+
+	global.Logger.Info("Initializing MySQL successfully")
+	global.Mdbc = db
+
+	// set Pool
+	SetPool()
+	migrateTables()
+	genTableDAO()
+}
+
+func SetPoolC() {
+	m := global.Config.Mysql
+	sqlDb, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Printf("mysql error: %s::", err)
+	}
+
+	sqlDb.SetConnMaxIdleTime(time.Duration(m.MaxIdleConns))
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime))
+}
+
+func genTableDAOC() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/models",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	// // gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(global.Mdb) // reuse your gorm db
+	// g.GenerateAllTable()
+	// g.GenerateModel("go_crm_user")
+	// // Generate basic type-safe DAO API for struct `model.User` following conventions
+	// g.ApplyBasic(model.User{})
+
+	// // Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	// g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+
+	// Generate the code
+	g.Execute()
+}
+
+func migrateTablesC() {
+	err := global.Mdb.AutoMigrate(
+		&po.User{},
+		&po.Role{},
+	)
+
+	if err != nil {
+		fmt.Println("Migration tables error:", err)
+	}
+}
--- internal/repo/user.repo.go
+++ internal/repo/user.repo.go
@@ -1,5 +1,10 @@
 package repo
 
+import (
+	"go/go-backend-api/global"
+	"go/go-backend-api/internal/database"
+)
+
 // type UserRepo struct{}
 
 // func NewUserRepo() *UserRepo {
@@ -17,14 +22,17 @@ type IUserRepository interface {
 }
 
 type userRepository struct {
+	sqlc *database.Queries
 }
 
 func NewUserRepository() IUserRepository {
-	return &userRepository{}
+	return &userRepository{
+		sqlc: database.New(global.Mdbc),
+	}
 }
 
 // GetUserByEmail implements IUserRepository.
-func (u *userRepository) GetUserByEmail(email string) bool {
+func (up *userRepository) GetUserByEmail(email string) bool {
 	// SELECT * FROM user WHERE email = '??' ORDER BY email
 	// row := global.Mdb.Table(TableNameGoCrmUser).Where("usr_email = ?", email).First(&model.GoCrmUser{}).RowsAffected
 	// user, err := up.sqlc.GetUserByEmailSQLC(ctx, email)
@@ -33,5 +41,11 @@ func (u *userRepository) GetUserByEmail(email string) bool {
 	// 	return false
 	// }
 
-	return false
+	user, err := up.sqlc.GetUserByEmailSQLC(ctx, email)
+
+	if err != nil {
+		return false
+	}
+
+	return user.UsrID != 0
 }
